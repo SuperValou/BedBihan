ef847a7dd1ab195d9ecd3eb4e99acc3f7f956d23
conicole <corentin.nicol@live.com>
2014-10-09 17:59:31 +0200

    amorce diagramme sequence

---
+++ BedBihan/SmallBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BedBihan
+{
+    public class SmallBoard : IBoard
+    {
+        public IEnumerable<Hexagon> grid
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void buildBoard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
