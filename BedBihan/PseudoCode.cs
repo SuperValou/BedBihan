aa2159f4c7c867a95cfa542cbf14f33db1f3ae72
conicole <conicole@insa-rennes.fr>
2014-11-03 11:40:28 +0100

    Sequencage des combat

---
+++ BedBihan/PseudoCode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+class Unit
+ * 
+void deplacer (Hexagon hexagon)
+{
+    this.deplacementPoint--;
+ * if (hexagon.isOccuped() )
+ * {
+ *      attack(hexagon.selectBestUnit());
+ * }
+}
+ * 
+ * void attack(Unit defender)
+ * {
+ *      int fightNumber = math.random(machin);
+ *      while (fightNumber > 0 || (this.lifePoint > 0 and defender.lifePoint > 0))
+ *      {
+ *         COOMMMMMMBAAAATTT
+ *      }
+ * }
+
+ * void selectBestUnit()
+ * {
+ *      
+ * }
+*/
