b48efd77cc1df2966e244db429abcb183bd2c678
SuperValou <valentin.esmieu@insa-rennes.fr>
2014-11-12 03:07:09 +0100

    beautiful report

---
+++ BedBihan/HexagonFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BedBihan
+{
+    public class HexagonFactory
+    {
+        public IEnumerable<Hexagon> hexagons
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public Hexagon getHexagons(String fieldType)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
