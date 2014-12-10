e637bc6d2621b86af24bd014492c62769a6a0aa4
unknown <vesmieu@PC3-INFO9.educ.insa>
2014-12-10 09:11:04 +0100

    push des textures

--- BedBihan/Coordinates.cs
+++ BedBihan/Coordinates.cs
@@ -13,6 +13,34 @@ namespace BedBihan
             this.x = x;
             this.y = y;
         }
+
+        /*
+         * return true if the coordinates are adjacent to the current coordinates (hexagonal grid)
+         * */
+        public bool isAdjacentTo(Coordinates coord)
+        {
+            if (this.x % 2 == 0)
+            // x is even
+            {
+                return     this.x == coord.x        && this.y == coord.y + 1    // left
+                        || this.x == coord.x + 1    && this.y == coord.y + 1    // top left
+                        || this.x == coord.x + 1    && this.y == coord.y        // top right
+                        || this.x == coord.x        && this.y == coord.y - 1    // right
+                        || this.x == coord.x - 1    && this.y == coord.y        // bottom right
+                        || this.x == coord.x - 1    && this.y == coord.y + 1;   // bottom left
+            }
+            else
+            // x is odd
+            {
+                return     this.x == coord.x        && this.y == coord.y + 1    // left
+                        || this.x == coord.x + 1    && this.y == coord.y        // top left
+                        || this.x == coord.x + 1    && this.y == coord.y - 1    // top right
+                        || this.x == coord.x        && this.y == coord.y - 1    // right
+                        || this.x == coord.x - 1    && this.y == coord.y - 1    // bottom right
+                        || this.x == coord.x - 1    && this.y == coord.y ;      // bottom left;
+            }
+                        
+        }
     }
     
 }
