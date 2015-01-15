c10d302de9aa2d31b74b91cbc400d34af7c9c69c
conicole <corentin.nicol@live.com>
2015-01-15 23:09:33 +0100

    update

--- BedBihanGUI/UnitScore..xaml.cs
+++ BedBihanGUI/UnitScore..xaml.cs
@@ -33,7 +33,7 @@ namespace BedBihanGUI
             unitTex = u;
             Race.Content = u.unit.faction.ToString();
             HP.Content = u.unit.currentHP;
-            HPMax.Content = "/ " + u.unit.maxHP;
+           
         }
 
         /*
